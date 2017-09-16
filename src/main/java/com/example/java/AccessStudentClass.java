f07c989c243ed0bb4318668e4eb6adf013706d46
Mohammed Maqsud Ansari <mohammed.ansari@sprinklr.com>
2017-09-16 18:20:39 +0530

    add

---
+++ src/main/java/com/example/java/AccessStudentClass.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+/**
+ * Created by maiq on 16/09/17.
+ */
+public class AccessStudentClass {
+
+    public static void main(String[] args) {
+
+        System.out.println(Student.rollNo+" "+Student.name+" "+Student.branch+" "+Student.college);
+    }
+}
