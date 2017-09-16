01946912ef7655506b7357d41f3753f551c84605
Mohammed Maqsud Ansari <mohammed.ansari@sprinklr.com>
2017-09-16 18:15:23 +0530

    add

---
+++ src/main/java/com/example/java/Student.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+/**
+ * Created by maiq on 16/09/17.
+ */
+public interface Student {
+
+    Long rollNo = 13576L;
+    String name = "Mohammad";
+    String branch = "CSE";
+    String college = "NIT Hamirpur";
+}
